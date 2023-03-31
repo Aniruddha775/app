eac69191ffc72fbaa78515c60f88266868ecd937
Aniruddha <aniruddhadchowdhury79@gmail.com>
2023-03-31 21:21:23 +0530

    FirstCommit

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,30 +11,40 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   dynamic _counter = 25;
   dynamic _ph = 7.5;
-  void _incrementCounter(){
-    if(_counter>=0 && _counter<100 && _counter != _ph){
+  bool isVisible = true;
+  void _incrementCounter() {
+    if (_counter >= 0 && _counter < 100 && _counter != _ph) {
       setState(() {
         _counter++;
       });
-    };
+    }
+    ;
   }
-  void _decrementCounter(){
-    if(_counter>15 && _counter != _ph){
+
+  void _decrementCounter() {
+    if (_counter > 15 && _counter != _ph) {
       setState(() {
         _counter--;
       });
-    };
+    }
+    ;
   }
-  void _pH(){
+
+  void _pH() {
     setState(() {
       _counter = _ph;
+      isVisible = false;
     });
   }
-  void _temperature(){
+
+  void _temperature() {
     setState(() {
       _counter = 25;
+      isVisible = true;
     });
   }
+
+  final databaseRef = FirebaseDatabase.instance.ref('Data');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,26 +66,59 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ),
                 SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _decrementCounter,
-                      child: Text('-'),
-                    ),
-                    SizedBox(width: 16.0),
-                    ElevatedButton(
-                      onPressed: _incrementCounter,
-                      child: Text('+'),
-                    ),
-                  ],
+                Visibility(
+                  visible: isVisible,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _decrementCounter,
+                        child: Text('-'),
+                      ),
+                      SizedBox(width: 16.0),
+                      ElevatedButton(
+                        onPressed: _incrementCounter,
+                        child: Text('+'),
+                      ),
+                    ],
+                  ),
                 ),
-                ElevatedButton(onPressed: _pH, child: Text("pH"),),
-                ElevatedButton(onPressed: _temperature, child: Text("Temperature"),),
+                ElevatedButton(
+                  onPressed: _pH,
+                  child: Text("pH"),
+                ),
+                ElevatedButton(
+                  onPressed: _temperature,
+                  child: Text("Temperature"),
+                ),
                 SizedBox(
                   height: 100,
                 ),
-                ElevatedButton(onPressed: null, child: Text("Start"),),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Start"),
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: FirebaseAnimatedList(
+                        queue: databaseRef,
+                        itemBuilder: (context, snapshot, animation, index) {
+                          return Column(
+                            children: [
+                              ListTile(
+                                title: Text(snapshot
+                                    .child('latitude')
+                                    .value
+                                    .toString()),
+                              ),
+                              ListTile(
+                                  title: Text(snapshot
+                                      .child('longitude')
+                                      .value
+                                      .toString())),
+                            ],
+                          );
+                        })),
               ],
             ),
           ),
